e459fad160ae3f621e2fbcdebcbf01b52f33ca3c
aki <forever.look.aki27@gmail.com>
2020-09-09 14:57:45 +0900

    上下に飾り付けを行うクラス追加

--- src/decorator/Main.java
+++ src/decorator/Main.java
@@ -5,8 +5,12 @@ public class Main {
 		Display d1 = new StringDisplay("Hello world");
 		Display d2 = new SideBorder(d1, '#');
 		Display d3 = new FullBorder(d2);
+		Display d4 = new UpDownBorder(d1, '-');
+		Display d5 = new UpDownBorder(d2, '-');
 		d1.show();
 		d2.show();
 		d3.show();
+		d4.show();
+		d5.show();
 	}
 }
